5958e6115c7b8b98172e5707b1dbfb40bb837e69
Eduarda Chagas <eduardamachagas@gmail.com>
2021-10-08 11:53:38 -0300

    primeira questao

--- JAVA/Questao 2/Teclado.java
+++ JAVA/Questao 2/Teclado.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Teclado {
     public static void main (String[] args) {
-        System.out.println("Inrisa");
-        Teclado input = new Scanner();
-        int inteiro;
-        String stringue;
-        inteiro = input.LerInteiro();
-        stringue = input.LerString();
+        Scanner scanner = new Scanner(System.in);
+        int inteiro1;
+        int inteiro2;
+        int inteiro3;
+
+        inteiro1 = scanner.nextInt();
+        inteiro2 = scanner.nextInt();
+        inteiro3 = scanner.nextInt();
+
+        System.out.println("" + inteiro1);
+        System.out.println(inteiro2);
+        System.out.println(inteiro3);
+
+        scanner.close();
     }
 }
