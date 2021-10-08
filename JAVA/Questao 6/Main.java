4e323fbd13b8dc76224e941fe4c8b1e686e1d164
EduardaAChagas <eduardamachagas@gmail.com>
2021-10-08 16:49:04 -0300

    questao 6

---
+++ JAVA/Questao 6/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main (String args[]) {
+        String hello = "Helloworld";
+        int start = 3;
+        int end = 7;
+        String novaStr;
+
+        novaStr = hello.substring(start,end);
+        System.out.println(novaStr);
+    }
+}
