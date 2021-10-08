936781af079041d2131472369e1214ace15945af
Eduarda Chagas <eduardamachagas@gmail.com>
2021-10-08 13:50:57 -0300

    att questao 5

--- JAVA/Questao 5/Main.java
+++ JAVA/Questao 5/Main.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
-        String myString = scanner.next();
+        String myString = scanner.nextLine();
 
-        int length;
-        length = myString.length();
-        System.out.println(myString.length());
+        int tamanho; 
+        tamanho = myString.length(); // ma ele so vai pegar o tamanho da primeira string
+       	System.out.println(myString.length()); //agora to
         scanner.close();
-    }
+  }
 }
