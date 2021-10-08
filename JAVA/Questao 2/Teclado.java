ce73bf3d8d7d43d729e1e33e84174b3c2dcc762c
Eduarda Chagas <eduardamachagas@gmail.com>
2021-10-08 11:35:49 -0300

    primeira questao

---
+++ JAVA/Questao 2/Teclado.java
@@ -0,0 +1,10 @@
+public class Teclado {
+    public static void main (String[] args) {
+        System.out.println("Inrisa");
+        Teclado input = new Scanner();
+        int inteiro;
+        String stringue;
+        inteiro = input.LerInteiro();
+        stringue = input.LerString();
+    }
+}
